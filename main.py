bcfc70baaf46a07cdd5a7a3215e36af4e85c3d08
alirz-pixel <alirce@naver.com>
2021-03-13 04:57:26 +0900

    First commit

---
+++ main.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+
+key = ""
+ListBox_index = 0
+
+def Add_Keyboard():
+    def key_down(e):
+        global key
+        key = e.keysym
+        print("키 입력 : " + str(key))
+
+    def main_proc():
+        AddKey_Text["text"] = key
+        win_AddKey.after(100, main_proc)
+
+    def Add_ListBox():
+        global ListBox_index
+        ListBox_index += 1
+        listBox.insert(ListBox_index, key)
+        win_AddKey.destroy()
+
+    win_AddKey = tk.Tk()
+    win_AddKey.title("키보드 설정")
+    win_AddKey.geometry("200x160")
+    win_AddKey.attributes('-toolwindow', True)
+    win_AddKey.bind("<KeyPress>", key_down) #키보드 입력 받기
+
+    AddKey_Label = tk.Label(master=win_AddKey, text='추가할 키를 입력해 주세요.')
+    AddKey_Label.place(x=23, y=10)
+
+    AddKey_Text = tk.Label(master=win_AddKey, text=' ', font=("System", 5), relief='sunken')
+    AddKey_Text.config(bg='LightGray')
+    AddKey_Text.place(x=25, y=33, width=150, height=32)
+
+    main_proc() #키보드 입력 받기
+
+    Add_Btn = tk.Button(master=win_AddKey, text="완료", command=Add_ListBox)
+    Add_Btn.place(x=75, y=120, width=60, height=25)
+
+    win_AddKey.mainloop()
+
+#tk 기본 설정
+win = tk.Tk()
+win.title("Py Macro")
+win.geometry("250x190")
+
+#키보드 추가 박스 생성
+Key_Box = tk.Button(win, text="키보드", borderwidth=2, command=Add_Keyboard)
+Key_Box.place(x=155, y=17, width=70, height=30)
+
+#마우스 추가 박스 생성
+Mouse_Box = tk.Button(win, text="마우스", borderwidth=2)
+Mouse_Box.place(x=155, y=52, width=70, height=30)
+
+#시간 추가 박스 생성
+Mouse_Box = tk.Button(win, text="시간", borderwidth=2)
+Mouse_Box.place(x=155, y=87, width=70, height=30)
+
+#지우기 박스 생성
+Mouse_Box = tk.Button(win, text="지우기", borderwidth=2)
+Mouse_Box.place(x=155, y=122, width=70, height=30)
+
+#listbox 생성
+listBox = tk.Listbox(win)
+listBox.place(x=13, y=17, width=130, height=140)
+
+tk.mainloop()
