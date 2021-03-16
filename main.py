918bcbfedc758b3fc09f297258d7fd0a0cc94b39
alirz-pixel <alirce@naver.com>
2021-03-17 04:37:31 +0900

    Adds a string to the Keyboard function input method.

--- main.py
+++ main.py
@@ -4,42 +4,80 @@ import tkinter as tk
 key = ""
 selected = -1
 special_keys={}
+str_Option = 0
 
 #키보드 추가 함수
 def Add_Keyboard():
-    def key_down(e):
-        global key
+    global key
 
-        if key != e.keysym:
-            key = e.keysym
-            AddKey_Text["text"] = key
-            print("키 입력 : " + str(key))
+    if key != "":
+        key = ""
+
+    def Change_Str():
+        global str_Option
+        if str_Option == 0:
+            str_Option = 1
+        else:
+            str_Option = 0
+
+        win_AddKey.destroy()
+        Add_Keyboard()
+
+
+    def key_down(e):
+        global str_Option
+
+        if str_Option == 0:
+            global key
+
+            if key != e.keysym:
+                key = e.keysym
+                AddKey_Text["text"] = key
+                print("키 입력 : " + str(key))
 
     def Add_ListBox():
-        listBox.insert(tk.END, key)
+        if key != "":
+            listBox.insert(tk.END, " key : " + key)
+        else:
+            listBox.insert(tk.END, " str : " + en_AddStr.get())
         win_AddKey.destroy()
 
     win_AddKey = tk.Tk()
     win_AddKey.title("키보드 설정")
     win_AddKey.geometry("200x160")
+    win_AddKey.lift() #창을 맨 위로 올리는 설정입니다.
+    win_AddKey.attributes("-topmost", True) #창을 맨 위로 올리는 설정입니다.
     win_AddKey.attributes('-toolwindow', True)
     win_AddKey.bind("<KeyPress>", key_down) #키보드 입력 받기
 
     AddKey_Label = tk.Label(master=win_AddKey, text='추가할 키를 입력해 주세요.')
     AddKey_Label.place(x=23, y=10)
 
-    AddKey_Text = tk.Label(master=win_AddKey, text=' ', font=("System", 5), relief='sunken')
-    AddKey_Text.config(bg='LightGray')
-    AddKey_Text.place(x=25, y=33, width=150, height=32)
+    if str_Option == 0:
+        AddKey_Text = tk.Label(master=win_AddKey, text=' ', font=("System", 5), relief='sunken')
+        AddKey_Text.config(bg='LightGray')
+        AddKey_Text.place(x=25, y=33, width=150, height=32)
+
+        bu_Change = tk.Checkbutton(master=win_AddKey, text='문자열 입력', command=Change_Str)
+        bu_Change.place(x=52, y=68)
+
+    else:
+        en_AddStr = tk.Entry(master=win_AddKey, font=("System", 5), relief='sunken', justify='center')
+        en_AddStr.config(bg='LightGray')
+        en_AddStr.place(x=25, y=33, width=150, height=32)
+
+        bu_Change = tk.Checkbutton(master=win_AddKey, text='키 입력', command=Change_Str)
+        bu_Change.place(x=52, y=68)
 
     Add_Btn = tk.Button(master=win_AddKey, text="완료", command=Add_ListBox)
-    Add_Btn.place(x=75, y=120, width=60, height=25)
+    Add_Btn.place(x=70, y=120, width=60, height=25)
 
     win_AddKey.mainloop()
 
 #ListBox 내용 삭제 함수
 def Delete_ListBox():
     global selected
+
     if selected >= 0:  # Check this still isn't -1
         listBox.delete(selected)
         selected = -1
@@ -52,7 +90,7 @@ def list_clicked(e):
     global selected
     selected = int(listBox.curselection()[0])  # item number selected in list
     item = listBox.get(selected)  # text of selected item
-    print(f"You have clicked item {selected} which is {item}")
+    print(f"You have clicked item {selected} which is {item} | type is {type(item)}")
 
 #특정 키를 눌렀을 때, 매크로 시작하기
 def Macro_Start():
@@ -60,19 +98,24 @@ def Macro_Start():
 
     if keyboard.is_pressed(special_keys['start']):
         for i in range(listBox.size()):
-            item = listBox.get(i)
-            print(f'now i is {i} | and | item is {item}')
+            item_Formet = listBox.get(i).split()[0]  #키 입력 또는 문자열 출력 등을 구분하는 변수
+            item_Content = listBox.get(i).split()[2] #리스트 박스에 들어있는 내용을 담는 변수
+            print(f'now i is {i} | and | item is {item_Formet}')
 
-            keyboard.press_and_release(item)
+            if item_Formet == 'str':
+                keyboard.write(item_Content)
+            elif item_Formet == 'key':
+                keyboard.press_and_release(item_Content)
+
 #옵션가져오기 
 def Get_Option():
     global special_keys
     try:
         f=open("option.txt",'r')
-        cup=list(f.readline().strip('\n'),split())
+        cup=list(f.readline().strip('\n').split())
         while cup!=list(''):
             special_keys[cup[0]]=cup[2]
-            cup=list(f.readline().strip('\n'),split())
+            cup=list(f.readline().strip('\n').split())
         f.close()
         print(special_keys)
     except: #첫 실행시 또는 옵션txt파일에 문제 있을 시 초기화후 실행
@@ -92,11 +135,12 @@ def Get_Option():
 win = tk.Tk()
 win.title("Py Macro")
 win.geometry("250x190")
+
 #옵션 가져오기
 Get_Option()
 
 #listbox 생성
-listBox = tk.Listbox(win)
+listBox = tk.Listbox(win, borderwidth=3)
 listBox.place(x=13, y=17, width=130, height=140)
 
 #selected is ListBox element
